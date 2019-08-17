2bf711cd0ceff95d2ea395d16524bb3dcf255c1d
Francisco Cruz <fcruz@codestack.co>
2019-08-16 18:00:55 -0700

    updated packages

---
+++ update.sh
@@ -0,0 +1,12 @@
+npm i --save @angular-devkit/architect@latest
+npm i --save @angular-devkit/build-angular@latest
+npm i --save @angular-devkit/build-optimizer@latest
+npm i --save @angular-devkit/build-webpack@latest
+npm i --save @angular-devkit/core
+npm i --save rxjs@latest
+npm i --save  @angular/cli@latest
+npm i --save  @ngtools/webpack@latest
+npm i --save  typescript@latest
+npm i --save  @angular/compiler-cli@latestnpm i --save 
+ng update @angular/cli @angular/core --allow-dirty
+ng update  --allow-dirty
